438eebfd9a482fe59ca600d8c013b2dfac9447b0
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-01-29 16:35:44 -0300

    Integraçao com o formulario

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -6,7 +6,7 @@ export class NegociacaoController {
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valaor');
+        this.inputValor = document.querySelector('#valor');
     }
 
     adiciona() {
