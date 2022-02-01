f933237e51569ad5319dcc7ed6c4cb77ca9e58a4
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-02-01 18:06:53 -0300

    Avançando no uso de generics

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,8 +1,16 @@
-export class View {
+export class View<T> {
     
     protected elemento: HTMLInputElement;
 
     constructor(seletor: string) {
         this.elemento = document.querySelector(seletor);
+    }
+
+    template(model: T): string {
+        throw Error('Classe filha precisa implementar o método template');
+    }
+    
+    update(model: T): void {
+        this.elemento.innerHTML = this.template(model);
     }
 }
