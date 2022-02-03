ac77d4c3b5e6ea394bac6aa19da009312178abe4
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-02-03 17:50:42 -0300

    Cuidados na declaraçao de enums

--- app/enums/dias-da-semana.ts
+++ app/enums/dias-da-semana.ts
@@ -1,9 +1,9 @@
 export enum DiasDaSemana {
-    DOM,
-    SEG,
-    TER,
-    QUA,
-    QUI,
-    SEX,
-    SAB
+    DOM = 0,
+    SEG = 1,
+    TER = 2,
+    QUA = 3,
+    QUI = 4,
+    SEX = 5,
+    SAB = 6
 }
