983de7fedc855c622f5dd7d9d5a6631cf822323f
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-02-05 16:02:17 -0300

    Protegendo nosso template

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,14 +1,22 @@
 export abstract class View<T> {
     
     protected elemento: HTMLInputElement;
+    private escapar = false;
 
-    constructor(seletor: string) {
+    constructor(seletor: string, escapar: boolean) {
         this.elemento = document.querySelector(seletor);
+        this.escapar = escapar;
     }
 
     protected abstract template(model: T): string;
     
     public update(model: T): void {
-        this.elemento.innerHTML = this.template(model);
+        let template = this.template(model);
+
+        if (this.escapar) {
+            template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+
+        this.elemento.innerHTML = template;
     }
 }
