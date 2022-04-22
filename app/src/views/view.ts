f80b78e85b26248b61b7d60067b556daeb0e1de0
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-04-21 21:38:06 -0300

    Ordem de execução dos decorators

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -18,8 +18,8 @@ export abstract class View<T> {
         }
     }   
 
-    @inspect()
     @logarTempoDeExecucao(false)
+    @inspect()
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
