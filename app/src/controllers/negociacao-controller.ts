09104ad3ca5b4f7f784f35ac9df942136162895b
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-04-23 16:38:37 -0300

    Criando nosso próprio dom injector

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
@@ -20,7 +21,8 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-    @logarTempoDeExecucao()
+    @inspect
+    @logarTempoDeExecucao(false)
     public adiciona(): void {
         const negociacao = Negociacao.criaDe(
             this.inputData.value, 
---
+++ app/src/decorators/dom-injector.ts
@@ -0,0 +1,8 @@
+export function domInjector(selector: string) {
+    return function(
+        target: any,
+        propertyKey: string
+    ) {
+        
+    }
+}
