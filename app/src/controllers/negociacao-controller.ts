91ff58a6844088db7c00f87ad13357434a7f3b5d
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-04-25 22:29:02 -0300

    Definindo uma interdace para API

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -2,6 +2,7 @@ import { domInjector } from '../decorators/dom-injector.js';
 import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { NegociacaoDoDia } from '../interfaces/nogociacao-do-dia.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
@@ -48,7 +49,7 @@ export class NegociacaoController {
     public importaDados(): void {
         fetch('http://localhost:8080/dados')
             .then(res => res.json())
-            .then((dados: any[]) => {
+            .then((dados: NegociacaoDoDia[]) => {
                 return dados.map(dadosDeHoje => {
                     return new Negociacao(
                         new Date(),
@@ -61,7 +62,7 @@ export class NegociacaoController {
                 for (let negociacao of negociacoesDeHoje) {
                     this.negociacoes.adiciona(negociacao);
                 }
-                
+
                 this.negociacoesView.update(this.negociacoes);
             });
     }
